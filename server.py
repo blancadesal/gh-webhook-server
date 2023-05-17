8d4568452faefac020e2e2280f2d1db3fb34792b
Slavina Stefanova <sstefanova@wikimedia.org>
2023-05-17 20:40:53 +0200

    add signature verification

--- server.py
+++ server.py
@@ -1,28 +1,38 @@
-from fastapi import FastAPI, Depends, HTTPException, Request
-from hmac import compare_digest, new
-from hashlib import sha1
+import hashlib
+import hmac
 import os
+
+from fastapi import Depends, FastAPI, HTTPException, Request
+
 
 app = FastAPI()
 
-# This is your secret token, which you'll also set in GitHub when configuring the webhook
-SECRET_TOKEN = os.environ.get('GITHUB_WEBHOOK_SECRET')
+async def get_payload_body(request: Request) -> bytes:
+    return await request.body()
 
-def verify_signature(request: Request, body: bytes, signature: str | None):
-    if signature is None:
-        raise HTTPException(status_code=400, detail='Missing X-Hub-Signature header')
+def get_signature_header(request: Request) -> str | None:
+    return request.headers.get("x-hub-signature-256")
 
-    expected_signature = new(SECRET_TOKEN.encode(), body, sha1).hexdigest()
-    if not compare_digest(f'sha1={expected_signature}', signature):
-        raise HTTPException(status_code=400, detail='Invalid signature')
+def verify_signature(payload_body: bytes = Depends(get_payload_body),
+                     signature_header: str | None = Depends(get_signature_header)) -> bool:
+    if not signature_header:
+        raise HTTPException(status_code=403, detail="x-hub-signature-256 header is missing!")
+
+    secret_token = os.environ.get('GITHUB_WEBHOOK_SECRET')
+
+    hash_object = hmac.new(secret_token.encode('utf-8'), msg=payload_body, digestmod=hashlib.sha256)
+    expected_signature = "sha256=" + hash_object.hexdigest()
+
+    if not hmac.compare_digest(expected_signature, signature_header):
+        raise HTTPException(status_code=403, detail="Request signatures didn't match!")
 
     return True
 
 @app.post("/webhook")
-async def handle_webhook(request: Request):
+async def handle_webhook(request: Request, verified: bool = Depends(verify_signature)):
     payload = await request.json()
 
     # Print the entire payload
-    print(payload)
+    print(payload['ref'])
 
     return {"message": "success"}
